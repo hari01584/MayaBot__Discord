5ad58a67bc47ceb1e4e55048b70dc24b3f55760d
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2020-12-25 13:49:10 +0530

    Added learning

--- bot/maya.py
+++ bot/maya.py
@@ -10,7 +10,10 @@ from COG.Economy import Economy
 from COG.Utility import Utility
 from COG.Naughty_Fun import Naughty_Fun
 
-client = commands.Bot(command_prefix=".",help_command=PrettyHelp())
+from ErrorHandling.ErrorHandler import ErrorHandler
+from AstraAstolosis.chatUtils import chatbot_response
+
+client = commands.Bot(command_prefix=commands.when_mentioned_or("."),help_command=PrettyHelp())
 token = loadToken();
 
 # TODO: ADD ALL COG HERE!!
@@ -18,6 +21,8 @@ token = loadToken();
 client.add_cog(Economy(client))
 client.add_cog(Utility(client))
 client.add_cog(Naughty_Fun(client))
+
+client.add_cog(ErrorHandler(client))
 #
 
 @client.event
@@ -25,6 +30,14 @@ async def on_ready() :
     await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
     print("I am online")
 
+@client.event
+async def on_message(message):
+    if client.user.mentioned_in(message):
+        text = message.clean_content.replace("@Maya Maxon","")
+        await message.channel.send(chatbot_response(text))
 
 
+    await client.process_commands(message)
+
+        
 client.run(token)
